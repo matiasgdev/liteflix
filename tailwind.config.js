bd002761ef302640da51cd679baccc5e43d4a2a1
Matias Navarro <matiasg.developer@gmail.com>
2023-06-27 18:36:00 -0300

    config: add prettier, eslint and tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontFamily: {
+          bebas: ['var(--bebas-neue)'],
+        },
+      },
+      colors: {
+        aqua: '#64EEBC',
+        white: ' #FFF',
+        black: ' #242424',
+        red: '#F00',
+      },
+    },
+  },
+  plugins: [],
+}
