7adb2376f5aed0048e6200829c2c4aee4f0f3e42
Matias Navarro <matiasg.developer@gmail.com>
2023-06-28 19:41:19 -0300

    feat: added hover state of Movie comonent

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,5 +1,6 @@
 import {useMovies} from '@/hooks/useMovies'
 import Arrow from '@public/icons/arrow.svg'
+import Star from '@public/icons/star.svg'
 import {PlayButton} from '@components/PlayButton'
 import Image from 'next/image'
 
@@ -18,9 +19,9 @@ export const MovieList = () => {
           {movies.map(movie => (
             <li
               key={movie.id}
-              className={`relative min-h-[146px] rounded overflow-hidden`}
+              className={`relative min-h-[146px] rounded overflow-hidden group cursor-pointer`}
             >
-              <div className="absolute inset-0 z-20 bg-gradient-to-b from-transparent  to-[#000]/[0.8]" />
+              <div className="absolute inset-0 z-20 bg-gradient-to-b from-transparent to-[#000]/[0.8] group-hover:bg-[#000]/[0.6]" />
               <Image
                 className="object-cover"
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
@@ -28,9 +29,18 @@ export const MovieList = () => {
                 fill
               />
               <PlayButton />
-              <h3 className="absolute bottom-2 z-30 translate-x-1/2 right-1/2 w-full px-4 text-center text-white font-medium tracking-widest truncate">
+              <h3 className="absolute max-w-max pt-1 bottom-2 z-30 translate-x-1/2 right-1/2 w-full px-4 text-center text-white font-medium tracking-widest truncate group-hover:bottom-1/2 group-hover:translate-y-[calc(50%+.5rem)] group-hover:translate-x-[7rem] group-hover:max-w-[180px] group-hover:text-left">
                 {movie.original_title}
               </h3>
+              <div className="absolute bottom-2 inset-x-0 z-30 hidden group-hover:flex justify-between px-4">
+                <div className="flex items-center gap-x-1 text-white">
+                  <Star />
+                  <span className="mt-1">{movie.vote_average}</span>
+                </div>
+                <div className="text-white">
+                  {movie.release_date.split('-').shift()}
+                </div>
+              </div>
             </li>
           ))}
         </ul>
