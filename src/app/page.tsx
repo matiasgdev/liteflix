c572eee0aab82f8a1f9f61554d056521a392365e
Matias Navarro <matiasg.developer@gmail.com>
2023-07-07 09:19:10 -0300

    feat: add gradient bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import {getHighlightMovie} from '@/services/movie-highlight.service'
 export default async function Home() {
   const highlightMovie = await getHighlightMovie()
   return (
-    <main className="max-w-6xl max-h-[80vh] md:min-h-screen md:max-h-max mr-auto ml-auto px-4">
+    <main className="relative max-w-6xl max-h-[80vh] md:min-h-screen md:max-h-max mr-auto ml-auto px-4">
+      <div className="absolute min-h-[60px]  bottom-0 left-0 right-0 z-[0] bg-gradient-to-b from-black/0 to-black md:hidden" />
       <ModalProvider
         background={<PosterBackground highlightMovie={highlightMovie} />}
       >
